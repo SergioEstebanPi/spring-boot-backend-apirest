6448ac68bc71bd236aeda3eda503343233611833
SergioEstebanPi <sepi_147@hotmail.com>
2018-11-25 11:41:23 -0500

    Creando controlador @RestController y Endpoint para listar

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+/* populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Gloria', 'Guzman', 'profesor@hotmail.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Andrea', 'Perez', 'profesor@hotmail.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Rosario', 'Plazas', 'profesor@hotmail.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Miguel', 'Rodriguez', 'profesor@hotmail.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Roberta', 'Velez', 'profesor@hotmail.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Gloria', 'Guzman', 'profesor@hotmail.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Gloria', 'Guzman', 'profesor@hotmail.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Gloria', 'Guzman', 'profesor@hotmail.com', '2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Gloria', 'Guzman', 'profesor@hotmail.com', '2018-01-01');
