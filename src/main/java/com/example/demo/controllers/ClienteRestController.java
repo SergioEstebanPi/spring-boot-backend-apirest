8057a04082894f3f555e699b39dbd35f7e087e70
SergioEstebanPi <sepi_147@hotmail.com>
2018-11-25 19:39:26 -0500

    Escribiendo los métodos update y delete en el controlador backend api rest

--- src/main/java/com/example/demo/controllers/ClienteRestController.java
+++ src/main/java/com/example/demo/controllers/ClienteRestController.java
@@ -1,16 +1,15 @@
 package com.example.demo.controllers;
 
-import java.util.Date;
 import java.util.List;
-
-import javax.persistence.PrePersist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -41,6 +40,22 @@ public class ClienteRestController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente create(@RequestBody Cliente cliente) {
 		return clienteServiceImpl.save(cliente);
+	}
+	
+	@PutMapping("/cliente/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente clienteActual = clienteServiceImpl.findbyId(id);
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setEmail(cliente.getEmail());
+		return clienteServiceImpl.save(clienteActual);
+	}
+	
+	@DeleteMapping("/cliente/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clienteServiceImpl.delete(id);
 	}
 
 }
